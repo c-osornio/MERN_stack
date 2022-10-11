fd4382617c6fa9cebc59d856d0a8d7e2578d1d83
Charlie <c.alejandro.oso@gmail.com>
2022-10-11 12:04:47 -0700

    updated

---
+++ Power_Up/Week2/10-6.js
@@ -0,0 +1,6 @@
+// What is the synthetic event used for submitting a form 🐪
+// onSubmit = { <<function>> }
+// Why is lifting state, what I mentioned as passing down state, so important ? 
+// So that developers are able to pass down values/attributes between components 
+// Why do we use e.preventDefault()
+//  we must prevent the default refresh of the browser to keep our state from being reset
