c8989e3b45f1e09974d1e23de85d3e960fab29c8
Charlie <c.alejandro.oso@gmail.com>
2022-10-14 18:13:01 -0700

    first commit

---
+++ React/CoinFlipping/client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Flip from './components/Flip.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Flip/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/CoinFlipping/client/src/components/Flip.js
@@ -0,0 +1,31 @@
+function tossCoin() {
+    return Math.random() > 0.5 ? "heads" : "tails";
+}
+
+function fiveHeads() {
+    return new Promise((resolve, reject) => {
+        let headsCount = 0;
+        let attempts = 0;
+        let maxFailures = 25;
+
+        while (headsCount < 5 && attempts <= maxFailures) {
+            attempts++;
+            let result = tossCoin();
+            console.log(`${result} was flipped`);
+            if (result === "heads") {
+                headsCount++;
+            } else {
+                headsCount = 0;
+            }
+        }
+        if (attempts <= maxFailures) {
+            resolve(`It took ${attempts} tries to flip five "heads"`)
+        } else {
+            reject(`Over ${maxFailures} failed attempts to flip "heads" 5 times in a row!`)
+        }
+    });
+}
+fiveHeads()
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+console.log("When does this run now?");
