86fcb79dd4fb002cdfa663f14f003f1ff37f8fe7
Charlie <c.alejandro.oso@gmail.com>
2022-10-14 20:35:25 -0700

    added axios, replaced fetch

--- React/PokemonAPI/client/src/App.js
+++ React/PokemonAPI/client/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Pokemon from './components/Pokemon'
+import React from 'react'
 
 function App() {
 
   return (
     <div className="App">
       <h1>All 905 Shiny Pokemon!</h1>
-      <Pokemon/>
+      <Pokemon />
     </div>
   );
 }
--- React/PokemonAPI/client/src/components/Pokemon.js
+++ React/PokemonAPI/client/src/components/Pokemon.js
@@ -1,22 +1,23 @@
+import React from 'react';
 import {useState, useEffect} from 'react'
 import styles from './Pokemon.module.css'
+import axios from 'axios';
 
 const Pokemon = () => {
 
-    const [state, setState] = useState([])
+    const [pokeball, setPokeball] = useState([])
     
     useEffect( () => {
-        fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=905')
-            .then(response=> response.json())
-            .then(response=> setState(response.results))
+        axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=905')
+            .then(response => setPokeball(response.data.results))
             .catch(err => console.log(err))
     } , [] )
 
 
     return (
-        <div className={styles.pokemon}>
+        <div className={styles.pokeball}>
             {
-                state.map((item,index) => (
+                pokeball.map((item,index) => (
                     <div key={index} className={styles.card} >
                         <h5>#{index + 1} {item.name.toUpperCase()}</h5>
                         <img src= {` https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${index + 1}.png `}  alt={item.name}/>
