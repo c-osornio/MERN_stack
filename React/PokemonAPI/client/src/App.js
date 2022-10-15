edcfbf323890428a39e02c1447c6911434aad88d
Charlie <c.alejandro.oso@gmail.com>
2022-10-14 18:54:22 -0700

    initial commit

---
+++ React/PokemonAPI/client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Pokemon from './components/Pokemon'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>All 905 Shiny Pokemon!</h1>
+      <Pokemon/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/PokemonAPI/client/src/components/Pokemon.js
@@ -0,0 +1,30 @@
+import {useState, useEffect} from 'react'
+import styles from './Pokemon.module.css'
+
+const Pokemon = () => {
+
+    const [state, setState] = useState([])
+    
+    useEffect( () => {
+        fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=905')
+            .then(response=> response.json())
+            .then(response=> setState(response.results))
+            .catch(err => console.log(err))
+    } , [] )
+
+
+    return (
+        <div className={styles.pokemon}>
+            {
+                state.map((item,index) => (
+                    <div key={index} className={styles.card} >
+                        <h5>#{index + 1} {item.name.toUpperCase()}</h5>
+                        <img src= {` https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${index + 1}.png `}  alt={item.name}/>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default Pokemon
