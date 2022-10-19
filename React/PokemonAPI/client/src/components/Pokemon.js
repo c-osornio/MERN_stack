131614a16a24bee8c8428e8d8d2e15ff3ab3c62c
Charlie <c.alejandro.oso@gmail.com>
2022-10-19 14:32:17 -0700

    added comments to show alt ways to consume API

--- React/PokemonAPI/client/src/components/Pokemon.js
+++ React/PokemonAPI/client/src/components/Pokemon.js
@@ -6,13 +6,38 @@ import axios from 'axios';
 const Pokemon = () => {
 
     const [pokeball, setPokeball] = useState([])
+
     
+//-------------------------------------------Fetch
+  // useEffect(()=>{
+  //   fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=905')
+  //   .then((result)=>{
+  //     return result.json()
+  //   }).then((res)=>{
+  //     console.log(res)
+  //     setPokeball(res.data)
+  //   }).catch((error)=>{
+  //     console.log(error)
+  //   })
+  // } , [])
+
     useEffect( () => {
         axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=905')
             .then(response => setPokeball(response.data.results))
             .catch(err => console.log(err))
     } , [] )
 
+    //-----------------------------------------------ES7 Async/Await
+
+//   const makeRequest = async () =>{
+//     try{
+//       const results = await axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=905')
+//       setState(results.data.data)
+//       console.log(results)
+//     } catch(error){
+//       console.log(error)
+//     }
+//   }
 
     return (
         <div className={styles.pokeball}>
