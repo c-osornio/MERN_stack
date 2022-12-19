814a8a9eef5ecd86eb49d76be793a8b340801346
Charlie <c.alejandro.oso@gmail.com>
2022-12-19 02:05:30 -0800

    updated assignment

--- algos/display.js
+++ algos/display.js
@@ -1,20 +1,52 @@
-// Display
-// Use classes, attributes, and methods in the JavaScript language for this challenge. Use only a single JavaScript file for this assignment, it may be the same from the Fronts assignment. All examples are done in order, starting with a new instance of the SLL class.
+class SLLNode {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class SLL {
+    constructor(val) {
+        this.head = null
+    }
+    addFront(value) {
+        var newNode = new SLLNode(value)
+        newNode.next = this.head;
+        this.head = newNode
+        return this.head
+    }
+    removeFront() {
+        if(this.head == null){
+            return this.head
+        }
+        var removedNode = this.head;
+        this.head = removedNode.next;
+        removedNode.next = null;
+        return this.head
+    }
+    front() {
+        return this.head ? this.head.value : null
+    }
+    display() {
+        var listValues = ""
+        if(this.head == null) {
+            return listValues
+        }
+        listValues += this.head.value;
+        var runner = this.head.next;
+        while (runner != null) {
+            listValues += ", " + runner.value;
+            runner = runner.next
+        }
+        return listValues
+    }
+}
 
 // Create display() that uses a while loop and a runner to return a string containing all list values. Build what you wish console.log(myList) did!
 
-//  class SLL {
-//     // constructor, other methods, removed for brevity
-//     display() {
-//     	// neatly display nodes in my list
-//     }
-//  }
-// copy
-// Examples:
-
-// SLL1 = new SLL()
-// SLL1.addFront(76) => Node { data: 76, next: null }
-// SLL1.addFront(2) => Node { data: 2, next: Node { data: 76, next: null } }
-// SLL1.display() => "2, 76"
-// SLL1.addFront(11.41) => Node { data: 11.41, next: Node { data: 2, next: Node { data: 76, next: null } } }
-// SLL1.display() => "11.41, 2, 76"
+SLL1 = new SLL()
+console.log(SLL1.addFront(76)) // Node { data: 76, next: null }
+console.log(SLL1.addFront(2)) // Node { data: 2, next: Node { data: 76, next: null } }
+console.log(SLL1.display()) // "2, 76"
+console.log(SLL1.addFront(11.41)) // Node { data: 11.41, next: Node { data: 2, next: Node { data: 76, next: null } } }
+console.log(SLL1.display()) // "11.41, 2, 76"
